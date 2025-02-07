69c3d0ed7fd8fd835f9ec141cd283c401d0ee953
saix511 <saadirfan41@gmail.com>
2025-02-08 01:50:57 +0500

    updated

---
+++ step_06_conditional_flows/conditional_and_flow.py
@@ -0,0 +1,42 @@
+import asyncio
+from crewai.flow.flow import Flow, listen, start, and_
+
+
+class OrExampleFlow(Flow):
+    @start()
+    def start_method(self):
+        print("Start Flow")
+        self.state["start"] = "Hello from the start method"
+        # return "Hello from the start method"
+
+    @listen(start_method)
+    async def second_method(self):
+        await asyncio.sleep(1)
+        print("Second Flow")
+        self.state["second"] = "Hello from the second method"
+        # return "Hello from the second method"
+
+    @listen(second_method)
+    async def third_method(self):
+        await asyncio.sleep(2)
+        print("Third Flow")
+        self.state["third"] = "Hello from the third method"
+        # return "Hello from the third method"
+
+    @listen(second_method)
+    async def fourth_method(self):
+        await asyncio.sleep(3)
+        print("fourth Flow")
+        self.state["forth"] = "Hello from the forth method"
+        # return "Hello from the fourth method"
+
+    @listen(and_(start_method, second_method, third_method, fourth_method))
+    def logger(self):
+        print(f"Logger Start: {self.state['start']}")
+        print(f"Logger Second: {self.state['second']}")
+        print(f"Logger Third: {self.state['third']}")
+        print(f"Logger Forth: {self.state['forth']}")
+
+
+flow = OrExampleFlow()
+flow.kickoff()
